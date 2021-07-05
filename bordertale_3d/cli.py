4e3502be95592c5e674411a92b7044be07938a11
Manas Mengle <manmenonsense@gmail.com>
2021-07-05 15:39:21 +0100

    cleaned up setup, added make run

--- bordertale_3d/cli.py
+++ bordertale_3d/cli.py
@@ -6,12 +6,11 @@ import sys
 def main():
     """Console script for bordertale_3d."""
     parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
+    parser.add_argument("_", nargs="*")
     args = parser.parse_args()
 
     print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "bordertale_3d.cli.main")
+    print("Replace this message by putting your code into " "bordertale_3d.cli.main")
     return 0
 
 
