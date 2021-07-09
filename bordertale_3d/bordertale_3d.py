655a67d0a45fc8e13924753f569215c425874e0d
appcreatorguy <manmenonsense@gmail.com>
2021-07-09 19:51:19 +0100

    added map rendering

--- bordertale_3d/bordertale_3d.py
+++ bordertale_3d/bordertale_3d.py
@@ -2,23 +2,29 @@
 import sys
 
 import pygame
-from pygame.constants import K_ESCAPE, KEYDOWN
+from pygame.constants import K_ESCAPE, KEYDOWN, RESIZABLE
 
 
 def main():
     """Run the main function of the game."""
     pygame.init()
     pygame.display.set_caption("Bordertale 3D")
-    screen = pygame.display.set_mode((900, 900), 0, 32)
+    screen = pygame.display.set_mode((600, 600), 0, 32)
     display = pygame.Surface((300, 300))
 
-    with open("map.txt") as f:
-        map_data = [
-            [int(c) for c in row] for row in f.read().split("\n")
-        ]  # Read Binary Map File
+    with open("data/map.txt") as f:
+        map_data = [[int(c) for c in row] for row in f.read().split("\n")]
+    f.close()  # * Read Binary Map File
 
     while True:
         display.fill((0, 0, 0))
+
+        for y, row in enumerate(map_data):
+            for x, tile in enumerate(row):
+                if tile:
+                    pygame.draw.rect(
+                        display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1
+                    )
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -29,5 +35,7 @@ def main():
                     pygame.quit()
                     sys.exit()
 
-        screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+        screen.blit(
+            pygame.transform.scale(display, screen.get_size()), (0, 0)
+        )  # * Screen Scaling
         pygame.display.update()
